d1673fb52720a515fbe6a0b166a6ba2a6bf56f08
Pijus <pijus.petkevicius@bentley.com>
2024-04-13 17:27:28 +0200

    cors

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ from fastapi import FastAPI
 from circuits import dummyCircuit, runExperiment
 from pydantic import BaseModel
 from qiskit_aer import AerSimulator
+from fastapi.middleware.cors import CORSMiddleware
 
 class Item(BaseModel):
     genome: str
     subgenome: str
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/genome/")
 async def create_item(item: Item):
